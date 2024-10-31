62bcfdc2fd3da12ee73ba9da744d8d216606b4a3
MateusPitura <mateuspitura@gmail.com>
2024-10-31 08:17:06 -0300

    feat: added nmcs

--- coloring/nmcs.py
+++ coloring/nmcs.py
@@ -2,13 +2,10 @@ import math
 import random
 import networkx as nx
 import read_dimacs
-import heapq
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import heapq # Fila de prioridade
+import copy # Para copiar o estado atual do grafo
 
-sequence = []
-
-class VertexQueue: # Cria uma fila de prioridade máxima para os vértices # TODO: teria que usar DSATUR também, porém a fila deveria alterar enquanto o grafo é colorido
+class VertexQueue: # Cria uma fila de prioridade máxima para os vértices pelo grau # TODO: teria que usar DSATUR também, porém a fila deveria alterar enquanto o grafo é colorido
   def __init__(self, graph):
     self.graph = graph
     self.queue = []
@@ -45,20 +42,20 @@ class State:
         self.coloring[vertex] = color
 
 def playout(state, policy, vertex_queue):
-    sequence.clear() # Deixa vazio o array, apenas para limpar quanto esse método for chamado várias vezes em sequência
-    while not state.is_terminal(): # Até todos os vértices serem coloridos
+    sequence = []
+
+    while True:
+        if state.is_terminal(): # Até todos os vértices serem coloridos
+            return score(state), sequence
+        
         possible_moves = state.get_possible_moves(vertex_queue.pop()) # Pega os movimentos possíveis para o maior vértice
 
-        z = sum([math.exp(policy[move]) for move in possible_moves]) # math.exp calcula a constante de euler elevada a x, que no geral será 0, resultadno em 1
-        move_probabilities = [math.exp(policy[move]) / z for move in possible_moves] # Retorna algo como [0.25, 0.25, 0.25, 0.25]
+        sum_total = sum([math.exp(policy[move]) for move in possible_moves]) # math.exp calcula a constante de euler elevada a x, que no geral será 0, resultadno em 1
+        move_probabilities = [math.exp(policy[move]) / sum_total for move in possible_moves] # Retorna algo como [0.25, 0.25, 0.25, 0.25]
         chosen_move = random.choices(possible_moves, weights=move_probabilities)[0] # Escolhe aleatoriamente um movimento baseado nas probabilidades, como serão iguais, qualquer um pode ser escolhido igualmente
 
         state.play(chosen_move)
         sequence.append(chosen_move)
-        yield state.coloring
-
-def get_final_result(state):
-    return score(state), sequence
 
 def score(state): # Verifica quantos conflitos existem no grafo, isto é, quantos vértices adjascentes são coloridos com as mesmas cores
     inconsistent_edges = 0
@@ -67,26 +64,32 @@ def score(state): # Verifica quantos conflitos existem no grafo, isto é, quanto
             inconsistent_edges += 1
     return state.graph.number_of_edges() - inconsistent_edges  # Quanto maior o score melhor, se o score for igual a quantidade de vértices então achamos uma solução
 
-def animate_coloring(graph, frames_function):
-    fig, ax = plt.subplots()
-    pos = nx.spring_layout(graph)
-    
-    def update(coloring):
-        ax.clear()
-        colors = [coloring.get(node, 'grey') for node in graph.nodes()]
-        nx.draw(graph, pos, node_color=colors, with_labels=True, node_size=500, cmap=plt.cm.jet, ax=ax)
-        ax.set_title("Graph Coloring")
+import random
+import networkx as nx
 
-    anim = FuncAnimation(fig, update, frames=frames_function(), interval=1000, repeat=False)
-    plt.show()
+def nmcs(state, policy, vertex_queue, level):
+    if level == 0:
+        return playout(state, policy, vertex_queue)
+
+    best_score = float('-inf')
+    best_sequence = []
+
+    while not state.is_terminal():
+        possible_moves = state.get_possible_moves(vertex_queue.pop())
+        for move in possible_moves:
+            state.play(move)
+            score, sequence = nmcs(copy.deepcopy(state), policy, copy.deepcopy(vertex_queue), level - 1)
+            if score > best_score:
+                best_score = score
+                best_sequence = sequence + [move]
+    return best_score, best_sequence
 
 def main():
     graph = read_dimacs.read_graph('instances/sample') # Lê um arquivo com as características do grafo e cria uma instância com networkx
     state = State(graph, 3)
     policy = {(vertex, color): 0 for vertex in state.graph.nodes() for color in range(state.max_colors)} # Irá retornar {(0, 0): 0, ..., (0, 2): 0}, onde o 1º elemento é o vértice e o 2º a cor, cada tupla terá a mesma probabilidade de ser escolhida 
     vertex_queue = VertexQueue(graph)
-    animate_coloring(graph, lambda: playout(state, policy, vertex_queue)) # TODO: retornar mais informações durante a coloração e o resultado final
-    print('playout', get_final_result(state)) # Irá retornar algo como (10, [(4, 2), (1, 2), (2, 1), (3, 0), (5, 3), (0, 0), (6, 1)]), onde 10 seria o score e as tuplas os vértices coloridos
+    print('nmcs', nmcs(state, policy, vertex_queue, 3)) # Irá retornar algo como (10, [(4, 2), (1, 2), (2, 1), (3, 0), (5, 3), (0, 0), (6, 1)]), onde 10 seria o score e as tuplas os vértices coloridos
 
 if __name__ == "__main__":
     main()
