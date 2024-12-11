40899b13fdc6f046a8ff79f87deb33dd6bc35c5b
MateusPitura <mateuspitura@gmail.com>
2024-12-10 21:21:55 -0300

    refactor: euleriano

--- euleriano/main.py
+++ euleriano/main.py
@@ -1,34 +1,60 @@
+"""
+    Verifica se um grafo é euleriano ou semi-euleriano
+"""
+
+
 class Grafo:
+    """
+        Classe que representa um grafo utilizando matriz de adjacências
+    """
+
     def __init__(self, vertices):
         self.vertices = vertices
         self.grafo = [[0]*self.vertices for i in range(self.vertices)]
 
-    def adiciona_aresta(self, u, v): 
+    def adiciona_aresta(self, u, v):
+        """
+            Adiciona uma aresta entre os vértices u e v
+        """
+
         self.grafo[u-1][v-1] += 1
-        if u != v: #Caso seja feito um laço, para não ficar repetido
+        if u != v:
             self.grafo[v-1][u-1] += 1
-    
+
     def mostra_grafo(self):
+        """
+            Mostra a matriz de adjacências
+        """
+
         print('A matriz de adjacências é: ')
-        for  i in range(self.vertices):
+        for i in range(self.vertices):
             print(self.grafo[i])
 
     def tem_aresta(self, u, v):
+        """
+            Verifica se existe aresta entre os vértices u e v
+        """
+
         if self.grafo[u-1][v-1] == 0:
             print(f'Não tem aresta entre {u} e {v}')
         else:
-            print(f'Existe {self.grafo[u-1][v-1]} de arestas entre os vértices {u} e {v}')
+            print(
+                f'Existe {self.grafo[u-1][v-1]} de arestas entre os vértices {u} e {v}')
 
-    def isEuleriano(self):
+    def is_euleriano(self):
+        """
+            Verifica se o grafo é euleriano ou semi-euleriano
+        """
+
         contador = 0
         for i in range(self.vertices):
             grau = 0
             for j in range(self.vertices):
-                if i == j: #Laço conta como 2 graus
+                if i == j:
                     grau += 2 * self.grafo[i][j]
                 else:
                     grau += self.grafo[i][j]
-            if grau % 2 != 0: #Verifica se é impar
+            if grau % 2 != 0:
                 contador += 1
         if contador == 0:
             print("É euleriano")
@@ -39,11 +65,8 @@ class Grafo:
 
 
 g = Grafo(4)
-
 g.adiciona_aresta(1, 2)
 g.adiciona_aresta(3, 4)
 g.adiciona_aresta(2, 3)
-
-g.isEuleriano()
-
+g.is_euleriano()
 g.mostra_grafo()
